d7a3b4f8b030d1def811ec31526faaf803c92a34
mwei2509 <m2dl89@gmail.com>
2017-02-28 01:29:12 -0500

    change env

--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,5 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
-configure :production do
-  db = URI.parse(ENV['HEROKU_POSTGRESQL_COPPER_URL'] || 'postgres://localhost/mydb')
-
-  ActiveRecord::Base.establish_connection(
-      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-      :host     => db.host,
-      :username => db.user,
-      :password => db.password,
-      :database => db.path[1..-1],
-      :encoding => 'utf8'
-      )
-end
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -86,4 +86,16 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/]
 
+  db = URI.parse(ENV['HEROKU_POSTGRESQL_COPPER_URL'] || 'postgres://localhost/mydb')
+
+  ActiveRecord::Base.establish_connection(
+      :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host     => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+      )
+
+
 end
